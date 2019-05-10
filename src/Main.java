3ada02cc517d2a217bc8c73374c5ec765aff3acf
YuriiSamila <43556791+YuriiSamila@users.noreply.github.com>
2019-05-10 22:19:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Insert your last name ");
         String last_name = sc.next();
         System.out.println(name + "\n" + last_name);
+        System.out.println("Finish");
     }
 }
