ed6ddac34293bd851a535321f3cecf1b92146081
YuriiSamila <beerdrink44@gmail.com>
2019-05-10 22:21:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Insert your name ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Insert your last name ");
+        String last_name = sc.next();
+        System.out.println(name + "\n" + last_name);
+    }
+}
